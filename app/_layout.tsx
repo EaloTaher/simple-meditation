e25d4585625e4d4cce0eb37231e1a1a7364ba982
Ealam Taher <ealozaher@gmail.com>
2024-11-25 14:04:17 +0300

    implemented the inital screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,8 @@
+//This componet works the same as children prop as web apps
+//basically it will render child route in it is place (ther index page)
+//fixes styling problems
+//any style in this file will work on all screens in this folder
+
 import { Slot } from "expo-router";
 
 // Import your global CSS file
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,45 @@
-import { Text, View } from "react-native";
-
-export default function HomeScreen() {
+import { View, Text, ImageBackground, SafeAreaView } from "react-native";
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+//make sure you use expo status bar and not react
+import { StatusBar } from "expo-status-bar";
+import beachImage from "@/assets/meditation-images/beach.webp";
+import CustomButton from "@/components/CustomButton";
+const App = () => {
   return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <Text>Hello World!</Text>
+    <View className="flex-1">
+      {/* Status bar can be anywhere in the component */}
+      <StatusBar style="light" />
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <LinearGradient
+          // using native wind makes some problems in LinearGradinet Component
+          style={{ flex: 1 }}
+          colors={["rgba(0, 0, 0, 0.4)", "rgba(0, 0, 0, 0.8)"]}
+        >
+          <SafeAreaView className=" flex-1 mx-5 my-8 justify-between">
+            <View>
+              <Text className="text-center text-white font-bold text-4xl">
+                Simple Meditation
+              </Text>
+              <Text className="text-center text-white text-regular text-2xl mt-3">
+                Simplifying Meditation for Everyone
+              </Text>
+            </View>
+            <View>
+              <CustomButton
+                onPress={() => console.log("Test")}
+                title="Get Started"
+              />
+            </View>
+          </SafeAreaView>
+        </LinearGradient>
+      </ImageBackground>
     </View>
   );
-}
+};
+
+export default App;
